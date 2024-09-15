a8f01f5ca84eb45f4b1e0d5560609d100c0402ea
Mostafa Mohamed Mousa <mostafamousa1080p@gmail.com>
2024-09-15 16:17:07 +0400

    Update model.py

--- app/model/model.py
+++ app/model/model.py
@@ -1,16 +1,11 @@
-import torch
+#MOCK model 
+
 from ultralytics import YOLO
 
 class ObjectDetectionModel:
-    def __init__(self, weights_path=None):
-        weights = weights_path if weights_path else "yolov5s.pt"
-        
-        # Check if GPU is available
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-        # Load the model on the selected device (CPU or GPU)
-        self.model = YOLO(weights).to(self.device)
+    def __init__(self):
+        self.model = YOLO("https://github.com/ultralytics/yolov5/releases/download/v6.2/yolov5s.pt")
 
     def predict(self, image_path):
-        results = self.model(image_path, device=self.device)
+        results = self.model(image_path)
         return results
