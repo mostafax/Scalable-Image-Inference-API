e4fcef58041a93d2cde635a831bb12310c037685
mostafax <mostafamousa1080p@gmail.com>
2024-09-15 10:57:32 -0400

    Fixing worker

--- app/worker.py
+++ app/worker.py
@@ -1,19 +1,20 @@
 from celery import Celery
-from model.model import ObjectDetectionModel
 from utils.helpers import parse_results
 import os
-
+from model.model import ObjectDetectionModel
 celery_app = Celery('worker', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
 
-model = ObjectDetectionModel()
 
 @celery_app.task(name='worker.process_image_task')
 def process_image_task(image_path):
     try:
+        
+        model = ObjectDetectionModel()
         results = model.predict(image_path)
         parsed_results = parse_results(results)
-        os.remove(image_path)  # Optinal here we can do verious actions 
         return parsed_results
     except Exception as e:
-        os.remove(image_path)
         return {'error': str(e)}
+
+if __name__ == "__main__":
+    celery_app.start()
